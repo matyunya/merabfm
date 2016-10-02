dd417b60a23cda04ad118a529cf6a773a35298d8
Maxim Matyunin <matyunya@gmail.com>
2016-10-02 18:33:28 +0300

    fuck tests

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import { IndexLink, Link } from 'react-router'
 import './Header.scss'
 
 export const Header = () => (
   <div>
     <h1 className="title">M E R A B  F M</h1>
-    {/*<IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>*/}
   </div>
 )
 
